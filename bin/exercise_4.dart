cc83c2b9ed415a60ccc9e68d1c58dbced9943581
unknown <berryterry914@gmail.comgit config --global user.name>
2024-02-20 19:38:52 +0100

    completed assignment

--- bin/exercise_4.dart
+++ bin/exercise_4.dart
@@ -6,11 +6,15 @@ void main() {
   List<Student> students = [student1, student2];
   //removing studentB
   // this would remove successfully cause it is referenced to the same object that I added to the list at line 6
-  students.remove(student2);
+
+  //Commenting on this and trying the second one
+  // students.remove(student2);
+  // print(students);
 
   //Removing the same studend but with the name and age instead
   //This is not referencing to the same object on the List, it's just creating a new instance using the class so theres no possible way this can work on that same List
   students.remove(Student("Erikefe", 34));
+  print(students);
 }
 
 //Creating the class
@@ -19,4 +23,10 @@ class Student {
   int age;
 
   Student(this.name, this.age);
+
+// to be able to see each students details instead of "[Instance of student]"
+  @override
+  String toString() {
+    return 'Student{name: $name, age: $age}';
+  }
 }
